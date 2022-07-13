a60cb858704910912ab233ae46edd48f314c4c2d
ATE <ATE@DESKTOP-ODIPDNU>
2022-07-13 11:02:47 +0900

    스프링 외부파일로 설정

--- src/main/java/com/haeun/test/AdminConnection.java
+++ src/main/java/com/haeun/test/AdminConnection.java
@@ -5,14 +5,26 @@ import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 
 public class AdminConnection implements InitializingBean {
-	private String adminId;
-	private String adminPw;
+	private String adminId;		// 관리자 Id
+	private String adminPw;		// 관리자 Pw
+	
+//	private Environment env;	//환경 변수 정의(설정)
+	
+//	@Override
+//	public void setEnvironment(Environment environment) {	// EnvironmentAware 상속
+//		// TODO Auto-generated method stub
+//		setEnv(environment);
+//	}
 	
 	
 	@Override
-	//초기화 된 후에 바로 실행되는 메소드
+	//초기화 된 후에 바로 (자동)실행되는 메소드
 	public void afterPropertiesSet() throws Exception {
+//		setAdminId(env.getProperty("adminId"));
+//		setAdminPw(env.getProperty("adminPw"));
 		
+//		setAdminId("adminId");
+//		setAdminPw("adminPw");
 	}
 
 	public String getAdminId() {
@@ -31,3 +43,4 @@ public class AdminConnection implements InitializingBean {
 		this.adminPw = adminPw;
 	}
 }
+
